e2bfc816be3ee5fdede13345b5bcee2e4481c022
Guilherme Welter <welter@ae.studio>
2023-04-12 16:48:44 -0300

    Introducing Structs, lists, closures and match

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Failed to read line");
     name.trim().to_lowercase()
 }
 
-fn was_person_invited(name: &String) -> bool {
-    let mut allow_them_in = false;
-    let visitors = ["bert", "steve", "freed"];
-    for visitor in &visitors {
-        if visitor == name {
-            allow_them_in = true;
-            break;
-        }
+fn was_person_invited(name: &str) {
+    let visitors = [
+        Visitor::new("Bob", "Hello, Bob!"),
+        Visitor::new("Alice", "Hello, Alice!"),
+        Visitor::new("John", "Hello, John!"),
+        Visitor::new("Jane", "Hello, Jane!"),
+    ];
+    let known_visitor = visitors.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not invited!")
     }
-    allow_them_in
 }
 
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {name:?}");
-    let was_invited = was_person_invited(&name);
-    if was_invited {
-        println!("Welcome to the party!");
-    } else {
-        println!("You're not invited!");
-    }
+    was_person_invited(&name);
 }
