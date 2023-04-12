a8fdc23a5a650df7841f2845e5c8694320a68a69
Guilherme Welter <welter@ae.studio>
2023-04-12 15:54:54 -0300

    Check if person was invited

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,26 @@ fn what_is_your_name() -> String {
     name.trim().to_lowercase()
 }
 
+fn was_person_invited(name: &String) -> bool {
+    let mut allow_them_in = false;
+    let visitors = ["bert", "steve", "freed"];
+    for visitor in &visitors {
+        if visitor == name {
+            allow_them_in = true;
+            break;
+        }
+    }
+    allow_them_in
+}
+
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {:?}", name);
+    println!("Hello, {name:?}");
+    let was_invited = was_person_invited(&name);
+    if was_invited {
+        println!("Welcome to the party!");
+    } else {
+        println!("You're not invited!");
+    }
 }
