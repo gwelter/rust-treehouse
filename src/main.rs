2bfae1133ff74fceb6056b7fd853f78140b24ba1
Guilherme Welter <welter@ae.studio>
2023-04-12 09:18:26 -0300

    Asking for user's name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::stdin;
+
+fn what_is_your_name() -> String {
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("Failed to read line");
+    name
+}
+
 fn main() {
     println!("Hello, what's your name?");
+    let name = what_is_your_name();
+    println!("Hello, {}", name);
 }
